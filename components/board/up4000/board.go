6a9341f8aae6bc3f0301b50937321874ce4b3de5
susmitaSanyal <susmita@viam.com>
2023-03-20 18:03:53 -0400

    pindef

---
+++ components/board/up4000/board.go
@@ -0,0 +1,26 @@
+// Package up4000 implements a up4000 based board.
+package up4000
+
+import (
+	"github.com/edaniels/golog"
+	"github.com/pkg/errors"
+	"periph.io/x/host/v3"
+
+	"go.viam.com/rdk/components/board/genericlinux"
+)
+
+const modelName = "up40000"
+
+func init() {
+	if _, err := host.Init(); err != nil {
+		golog.Global().Debugw("error initializing host", "error", err)
+	}
+
+	gpioMappings, err := genericlinux.GetGPIOBoardMappings(modelName, boardInfoMappings)
+	var noBoardErr genericlinux.NoBoardFoundError
+	if errors.As(err, &noBoardErr) {
+		golog.Global().Debugw("error getting up4000 GPIO board mapping", "error", err)
+	}
+
+	genericlinux.RegisterBoard(modelName, gpioMappings, true)
+}
---
+++ components/board/up4000/data.go
@@ -0,0 +1,24 @@
+package up4000
+
+import "go.viam.com/rdk/components/board/genericlinux"
+
+const bbAi = "bb_Ai64"
+
+var boardInfoMappings = map[string]genericlinux.BoardInformation{
+	bbAi: {
+		PinDefinitions: []genericlinux.PinDefinition{
+
+			{map[int]int{28: 5}, map[int]string{}, "600000.gpio", 29, 0, "GPIO10", "", "", -1},
+			{map[int]int{28: 6}, map[int]string{}, "600000.gpio", 31, 0, "GPIO11", "", "", -1},
+			{map[int]int{28: 14}, map[int]string{}, "600000.gpio", 8, 0, "GPIO15", "", "", -1},
+			{map[int]int{28: 15}, map[int]string{}, "600000.gpio", 10, 0, "GPIO16", "", "", -1},
+			{map[int]int{28: 16}, map[int]string{}, "600000.gpio", 36, 0, "GPIO25", "", "", -1},
+			{map[int]int{28: 23}, map[int]string{}, "600000.gpio", 16, 0, "GPIO18", "", "", -1},
+			{map[int]int{28: 24}, map[int]string{}, "600000.gpio", 18, 0, "GPIO19", "", "", -1},
+			{map[int]int{28: 25}, map[int]string{}, "600000.gpio", 22, 0, "GPIO20", "", "", -1},
+			{map[int]int{28: 26}, map[int]string{}, "600000.gpio", 37, 0, "GPIO14", "", "", -1},
+			{map[int]int{28: 27}, map[int]string{}, "600000.gpio", 13, 0, "GPIO4", "", "", -1},
+		},
+		Compats: []string{"up4000, UP-APL03P4F-A10-0864"},
+	},
+}
